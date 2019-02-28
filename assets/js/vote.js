834c074333fcc5996e568404b1cfd2d9d9066415
Klassare <klassare@gmail.com>
2019-02-28 21:38:09 +0100

    add voting

---
+++ assets/js/vote.js
@@ -0,0 +1,18 @@
+$('document').ready(function(){
+	updateVotes();
+});
+function showMsg(m){
+  alert(m);
+}
+function updateVotes(){
+  $.ajax({
+    type: "GET",
+    url: "https://rpc.tezrpc.me/chains/main/blocks/head/votes/proposals",
+    success: function(d){
+         $('#a1').html(d[0][0]);
+		 $('#a2').html(d[0][1]);
+		 $('#b1').html(d[1][0]);
+		 $('#b2').html(d[1][1]);
+    }
+  });
+}
