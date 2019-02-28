2e38f1fa2f98e1a330629c860b8324d7b44d8406
Klassare <klassare@gmail.com>
2019-02-28 22:17:39 +0100

    add TBD

--- assets/js/vote.js
+++ assets/js/vote.js
@@ -5,6 +5,7 @@ function showMsg(m){
   alert(m);
 }
 function updateVotes(){
+	var totalVotes = 51604;
   $.ajax({
     type: "GET",
     url: "https://rpc.tezrpc.me/chains/main/blocks/head/votes/proposals",
@@ -13,6 +14,8 @@ function updateVotes(){
 		 $('#a2').html(d[1][1]);
 		 $('#b1').html(d[0][0]);
 		 $('#b2').html(d[0][1]);
+		 $('#c2').html(totalVotes - d[0][1] - d[1][1])
+		 $('#d2').html(totalVotes)
     }
   });
 }
