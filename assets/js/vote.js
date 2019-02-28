37c90bc9f7db329a2a17f62d02bc2d3e3cc61d5f
Klassare <klassare@gmail.com>
2019-02-28 22:01:59 +0100

    add roll size

--- assets/js/vote.js
+++ assets/js/vote.js
@@ -9,10 +9,10 @@ function updateVotes(){
     type: "GET",
     url: "https://rpc.tezrpc.me/chains/main/blocks/head/votes/proposals",
     success: function(d){
-         $('#a1').html(d[0][0]);
-		 $('#a2').html(d[0][1]);
-		 $('#b1').html(d[1][0]);
-		 $('#b2').html(d[1][1]);
+         $('#a1').html(d[1][0]);
+		 $('#a2').html(d[1][1]);
+		 $('#b1').html(d[0][0]);
+		 $('#b2').html(d[0][1]);
     }
   });
 }
