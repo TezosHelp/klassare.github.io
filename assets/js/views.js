3dc9a685348982a90bf4c7d0f9114ae6950db40f
Klassare <klassare@gmail.com>
2019-06-24 00:09:13 +0200

    put noob questions on seperate url

--- assets/js/views.js
+++ assets/js/views.js
@@ -1,39 +1,31 @@
 window.onload = function(){
-	mainView();
+	//mainView();
 }
 mainView = function(){
 	get('container2').style.display = 'none';
-	get('container3').style.display = 'none';
 	get('container1').style.display = 'block';
 	window.scrollTo(0, 0);
 }
 mainViewDown = function(){
-	get('container3').style.display = 'none';
 	get('container2').style.display = 'none';
 	get('container1').style.display = 'block';
 	window.scrollTo(0,document.body.scrollHeight);
 }
 contactView = function(){
 	resetForm();
-	get('container3').style.display = 'none';
 	get('container1').style.display = 'none';
 	get('container2').style.display = 'block';
 	window.scrollTo(0, 0);
 }
-noobView = function(){
-	resetForm();
-	get('container2').style.display = 'none';
-	get('container1').style.display = 'none';
-	get('container3').style.display = 'block';
-	window.scrollTo(0, 0);
-}
 function resetForm(){
 	get('name').value = "";
-	get('name2').value = "";
-	get('question').value = "";
 	get('email').value = "";
 	get('message').value = "";
 	get('category').selectedIndex = 0;
+}
+function resetForm2(){
+	get('name2').value = "";
+	get('question').value = "";
 }
 function setContactView(){
 	contactView();
@@ -45,10 +37,6 @@ function setMainView(){
 }
 function setMainViewUp(){
 	mainView();
-	return false;
-}
-function setNoobView(){
-	noobView();
 	return false;
 }
 	//Help functions general
